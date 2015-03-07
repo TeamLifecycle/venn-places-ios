a8212680cf1529d290a25576a871e030dfcfec91
Steven White <swhitewvu24@gmail.com>
2015-03-07 13:51:15 -0500

    Update tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,37 +6,23 @@
 //  Copyright (c) 2014 Steven White. All rights reserved.
 //
 
+#import <VennPlaces/VennPlaces.h>
 SPEC_BEGIN(InitialTests)
 
-describe(@"My initial tests", ^{
-
-  context(@"will fail", ^{
-
-      it(@"can do maths", ^{
-          [[@1 should] equal:@2];
-      });
-
-      it(@"can read", ^{
-          [[@"number" should] equal:@"string"];
-      });
-    
-      it(@"will wait and fail", ^{
-          NSObject *object = [[NSObject alloc] init];
-          [[expectFutureValue(object) shouldEventually] receive:@selector(autoContentAccessingProxy)];
-      });
-  });
-
-  context(@"will pass", ^{
-    
-      it(@"can do maths", ^{
-        [[@1 should] beLessThan:@23];
-      });
-    
-      it(@"can read", ^{
-          [[@"team" shouldNot] containString:@"I"];
-      });  
-  });
-  
+describe(@"VennPlaces", ^{
+    context(@"API", ^{
+        beforeAll(^{
+            [VennPlaces setupWithApiKey:@"fookey"];
+        });
+        
+        it(@"should perform a search", ^{
+            NSDictionary *query = [NSDictionary dictionaryWithObjectsAndKeys:@"chinese", @"keyword", @"food", @"category", nil];
+            [[VennPlaces sharedPlaces] searchWithQuery:query andCallback:^(NSURLResponse *res, NSData *data, NSError *err) {
+                NSLog(@"%@", err);
+                NSLog(@"%@", data);
+            }];
+        });
+    });
 });
 
 SPEC_END
