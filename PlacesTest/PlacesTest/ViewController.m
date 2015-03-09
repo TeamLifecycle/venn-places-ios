b081520bf8332f14a4fe5f04edaa0abfa9fd7e01
Steven White <swhitewvu24@gmail.com>
2015-03-09 09:55:48 -0400

    Rework companion test project to be an application

---
+++ PlacesTest/PlacesTest/ViewController.m
@@ -0,0 +1,55 @@
+//
+//  ViewController.m
+//  PlacesTest
+//
+//  Created by Steven White on 3/7/15.
+//  Copyright (c) 2015 Strap. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <VennPlaces/VennPlaces.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController {
+    NSArray *places;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSDictionary *query = [NSDictionary dictionaryWithObjectsAndKeys:@"chinese", @"keyword",
+                           @"cincinnati oh 45202", @"address", nil];
+    
+    [[VennPlaces sharedPlaces] searchWithQuery:query andCallback:^(NSDictionary *data, NSError *e) {
+        places = [data valueForKeyPath:@"places"];
+        [self.table reloadData];
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [places count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *place = (NSDictionary *)[places objectAtIndex:indexPath.row];
+    NSLog(@"have place: %@", place);
+    NSString *name = [place objectForKey:@"name"];
+    NSLog(@"have name: %@", name);
+    
+    // Get Cell
+    NSLog(@"Getting cell");
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"places"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"places"];
+    }
+    
+    NSLog(@"Return cell");
+    cell.textLabel.text = name;
+    return cell;
+}
+
+@end
